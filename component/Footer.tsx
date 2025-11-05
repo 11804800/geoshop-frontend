2bf7f3ca28bca1f121ecc428e3e57de5dfa1d233
Nikhil Pathak <nikhilpathak2602@gmail.com>
2025-11-05 16:33:33 +0530

    Footer Component

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import Image from 'next/image'
+import { Github, Instagram, Linkedin } from "@deemlol/next-icons";
 
 function Footer() {
     return (
-        <div className='w-full h-10 bg-black text-white p-2'>Footer</div>
+        <div className='w-full h-full bg-black text-white py-6 px-12 flex flex-col sm:flex-row justify-center items-start'>
+            <div className='w-full sm:w-[400px] p-5  sm:border-r border-white flex justify-center sm:justify-normal flex-col gap-1'>
+                <Image src="/favicon.png" alt="footer_logo" width={200} height={200} className='self-center' />
+                <p className='text-justify text-zinc-300 text-sm'>
+                    Discover a wide selection of high-quality products at unbeatable prices. Shop confidently with fast shipping and secure checkout. Your one-stop destination for all your shopping needs!
+                </p>
+            </div>
+            <div className='w-full sm:w-[320px] h-80 sm:h-full flex flex-col justify-center items-center sm:items-start p-5 px-15'>
+                <h1>Social Media Links</h1>
+                <div className='flex gap-3 pt-4'>
+                    <a href="https://www.instagram.com/lucifer60_2/" target='blank' className='border-white border p-1 rounded-full hover:bg-white hover:text-black'><Instagram size={18} /></a>
+                    <a href='https://www.linkedin.com/in/nikhilpathak602/' target='blank' className='border-white border p-1 rounded-full hover:bg-white hover:text-black'><Linkedin size={18} /></a>
+                    <a href='https://github.com/11804800' target='blank' className='border-white border p-1 rounded-full hover:bg-white hover:text-black'><Github size={18} /></a>
+                </div>
+            </div>
+        </div>
     )
 }
 
